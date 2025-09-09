9ce52a3648469a66ea7c9ccf7d6cbac90e15dbe3
AVas <vao1404@gmail.com>
2025-09-09 22:10:21 +0300

    fix compare

--- src/compare.js
+++ src/compare.js
@@ -3,11 +3,11 @@ import _ from 'lodash'
 const buildData = (data1, data2) => {
   const keys1 = Object.keys(data1)
   const keys2 = Object.keys(data2)
-  const keys = _.union(keys1, keys2).sort() 
+  const keys = _.union(keys1, keys2).sort()
 
-  return keys.map((key) => { 
-    const value1 = data1[key] 
-    const value2 = data2[key] 
+  return keys.map((key) => {
+    const value1 = data1[key]
+    const value2 = data2[key]
 
     if (!Object.hasOwn(data1, key)) {
       return { type: 'added', key, value: data2[key] }
@@ -20,7 +20,7 @@ const buildData = (data1, data2) => {
     }
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
       return { type: 'nested', key, value: buildData(value1, value2) }
-    } 
+    }
     return {
       type: 'changed', key, value1: data1[key], value2: data2[key],
     }
